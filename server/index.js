54eccbf64f95d7982632e0f12a0e73fdddb4593f
TANUSH KHARE <tanushkhare0406@gmail.com>
2026-06-07 03:29:08 +0530

    Fix backend deployment

--- server/index.js
+++ server/index.js
@@ -94,8 +94,4 @@ app.use(errorHandler);
    SERVER
 ========================================== */
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+export default app;
